bff6815038c2ebddd9c4446bf634834fa205ae61
Sohail Shaikh <sohailhacker7786@gmail.com>
2024-01-21 18:41:23 +0530

    page 2 work

--- main.js
+++ main.js
@@ -32,9 +32,35 @@ function horizontalScroll() {
       trigger: ".container",
       pin: true,
       scrub: 1,
-      snap: 1 / (sections.length - 1),
+      // snap: 1 / (sections.length - 1),
       end: () => "+=" + document.querySelector(".container").offsetWidth,
     },
   });
 }
 horizontalScroll();
+
+var cursor = document.querySelector(".cursor");
+var project = document.querySelector(".project");
+
+function projectAnime() {
+  project.addEventListener("mouseenter", () => {
+    gsap.to(cursor, { backgroundColor: "#000", duration: 0.3 });
+  });
+  project.addEventListener("mouseleave", () => {
+    gsap.to(cursor, { backgroundColor: "#fff", duration: 0.3 });
+  });
+}
+projectAnime();
+
+function pageTl() {
+  // Create a GSAP Timeline
+  const timeline = gsap.timeline();
+  timeline.from(".main-home .top", { y: -300, duration: 1, delay: 3.5 });
+  timeline.from("#pp", {
+    opacity: 0,
+    y: 200,
+    duration: 1,
+  });
+  timeline.from(".home-footer", { y: 100, duration: 0.5 });
+}
+pageTl();
