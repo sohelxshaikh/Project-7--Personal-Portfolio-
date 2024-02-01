e2f4ebedb6107f44439b44bb995272c447e8218e
sohelxshaikh <sohailhacker7786@gmail.com>
2024-02-01 10:54:15 +0530

    modified:   style.css

--- main.js
+++ main.js
@@ -29,13 +29,13 @@ function horizontalScroll() {
   let sections = gsap.utils.toArray(".panel");
 
   gsap.to(sections, {
-    xPercent: -100 * (sections.length - 1),
+    xPercent: -100 * (sections.length -1),
     ease: "none",
     scrollTrigger: {
       trigger: ".container",
       pin: true,
-      scrub: 1,
-      // snap: 1 / (sections.length - 1),
+      scrub: 1.5,
+      // snap: 1 / (sections.length ),
       end: () => "+=" + document.querySelector(".container").offsetWidth,
     },
   });
@@ -167,8 +167,3 @@ function hamBurger(){
 hamBurger();
 
 
-function sheryShreylady(){
- 
-
-}
-sheryShreylady();
